eabcd7d31e2bbca1f43fb4e07ea2090cbd21d8b8
Florine <florine.cercle+perso@gmail.com>
2022-03-04 12:34:37 +0100

    first refactor of store.js: creation of several functions to update values in DiscountOffer

--- store.js
+++ store.js
@@ -4,6 +4,71 @@ export class DiscountOffer {
     this.expiresIn = expiresIn;
     this.discountInPercent = discountRateInPercent;
   }
+
+  updateExpiresIn() {
+    switch (this.partnerName) {
+      case "Ilek":
+        break;
+      default:
+        this.expiresIn--;
+    }
+  }
+
+  updateDiscount() {
+    switch (this.partnerName) {
+      case "Naturalia":
+        this.discountInPercent = this.getNewNaturaliaDiscount();
+        break;
+      case "Vinted":
+        this.discountInPercent = this.getNewVintedDiscount();
+        break;
+      case "Ilek":
+        break;
+      default:
+        this.discountInPercent = this.getNewDefaultDiscount();
+    }
+  }
+
+  getNewDefaultDiscount() {
+    if (this.expiresIn <= 0) {
+      return this.getDecreasedDiscount(2);
+    }
+    return this.getDecreasedDiscount(1);
+  }
+
+  getNewNaturaliaDiscount() {
+    if (this.expiresIn <= 0) {
+      return this.getIncreasedDiscount(2);
+    }
+    return this.getIncreasedDiscount(1);
+  }
+
+  getNewVintedDiscount() {
+    if (this.expiresIn <= 10 && this.expiresIn > 5) {
+      return this.getIncreasedDiscount(2);
+    } else if (this.expiresIn <= 5 && this.expiresIn > 0) {
+      return this.getIncreasedDiscount(3);
+    } else if (this.expiresIn <= 0) {
+      return 0;
+    }
+    return this.getIncreasedDiscount(1);
+  }
+
+  getIncreasedDiscount(increment) {
+    const incrementedDiscount = this.discountInPercent + increment;
+    if (incrementedDiscount <= 50) {
+      return incrementedDiscount;
+    }
+    return 50;
+  }
+
+  getDecreasedDiscount(increment) {
+    const decrementedDiscount = this.discountInPercent - increment;
+    if (decrementedDiscount >= 0) {
+      return decrementedDiscount;
+    }
+    return 0;
+  }
 }
 
 export class Store {
@@ -11,54 +76,9 @@ export class Store {
     this.discountOffers = discountOffers;
   }
   updateDiscounts() {
-    for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-          }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
-          }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          }
-        }
-      }
+    for (let discountOffer of this.discountOffers) {
+      discountOffer.updateDiscount();
+      discountOffer.updateExpiresIn();
     }
 
     return this.discountOffers;
